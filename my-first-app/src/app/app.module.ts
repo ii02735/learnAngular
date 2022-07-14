484571e849205a1d10b0f28ceb880ff0627f30ff
ii02735 <yadallee.bilaal@outlook.fr>
2022-07-14 22:17:30 +0200

    [ANGULAR] Creating second component with CLI

--- my-first-app/src/app/app.module.ts
+++ my-first-app/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { FirstComponent } from './first-component/first-component.component';
+import { SecondComponentComponent } from './second-component/second-component.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    FirstComponent
+    FirstComponent,
+    SecondComponentComponent
   ],
   imports: [
     BrowserModule

2b4469cc50427dad072a9098eb9cd5685791ab78
Bilaal Yadallee <bilaal.yadallee@insign.fr>
2022-07-17 12:23:38 +0200

    [ANGULAR] Using html attribute selector + applying interpolation

--- my-first-app/src/app/first-component/first-component.component.ts
+++ my-first-app/src/app/first-component/first-component.component.ts
@@ -1,10 +1,18 @@
 import { Component } from "@angular/core";
 
 @Component({
-    selector: "app-first-component",
+    selector: "[app-first-component]",
     templateUrl: "./first-component.component.html",
     styleUrls: []
 })
 export class FirstComponent {
+    
+    status: boolean = true;
 
+    otherData: string = "hello world first component !"
+
+    statusOutput(): string {
+
+        return this.status ? "Value is true" : "Value is false"
+    }
 }

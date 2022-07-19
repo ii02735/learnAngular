bd4ef891325423aa7c2dd26e790c1bab6df87f10
Bilaal Yadallee <bilaal.yadallee@insign.fr>
2022-07-19 21:08:25 +0200

    [ANGULAR] Component : data-binding through method

--- my-first-app/src/app/second-component/second-component.component.ts
+++ my-first-app/src/app/second-component/second-component.component.ts
@@ -8,12 +8,22 @@ import { Component, OnInit } from '@angular/core';
 export class SecondComponentComponent implements OnInit {
 
   allowedAction: boolean = false;
+  isLogged: boolean = false;
+
 
   constructor() {
     // Après 4 secondes, on change la valeur de la propriété
     // Et donc le COMPORTEMENT du bouton derrière, ce dernier 
     // comportant une property-binding sur cet attribut, pour disabled
     setTimeout(() => { this.allowedAction = true },4000)
+  }
+
+  onClickLoginButton() {
+    this.isLogged = true;
+  }
+
+  returnLoggedStatus() {
+    return this.isLogged ? "user is logged !" : "user is not logged"
   }
 
   ngOnInit(): void {
